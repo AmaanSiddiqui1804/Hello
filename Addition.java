566b6cb25667238c2eeac0ba25cbf383a3727ede
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-01-07 00:43:57 +0530

    Added Addition program to main repository

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition{
+   public static void main(String [] args){ 
+   System.out.print("Addition program of two numbers:");
+   
+   int a = 10;
+   int b = 15;
+   int sum = a+b;
+   
+   System.out.print("The sum of 10 and 15 is: " + sum);
+ }
+}
