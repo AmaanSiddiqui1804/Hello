238a10d525b219c0b7a083164ad8a440d48cba69
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-01-07 09:58:54 +0530

    Addition functionality added to branch addition

--- Addition.java
+++ Addition.java
@@ -1,49 +1,9 @@
-import java.util.*;
-public class Calculator{
+public class Addition{
    public static void main(String [] args){ 
-   Scanner sc = new Scanner(System.in);
-   System.out.print("Enter first number: ");
-   int a = sc.nextInt();
-   System.out.println();
-   System.out.print("Enter second number: "); 
-   int b = sc.nextInt();
-   System.out.println("Enter name of Operation:" );
-   System.out.println("add || subtract || product || division");
-   String op = sc.next();
-   
-   int sum, dif, mul, div;
-
-   if ("add".equalsIgnoreCase(op)) {
       
-         sum = a+b;
-         System.out.println("Addition of the given numbers is: " + sum);
+     int a = 6;
+     int b = 16;
+     int sum = a+b;
+     System.out.println("the sum of 6 and 16 is: " + sum);
    }
-
-   else if ("subtract".equalsIgnoreCase(op)) {
-      
-         dif = a-b;
-         System.out.println("Difference of the given numbers is: " + dif);
-   }
-   
-   else if ("product".equalsIgnoreCase(op)) {
-      
-         mul = a*b;
-         System.out.println("Product of the given numbers is: " + mul);
-   }
-
-   else if ("division".equalsIgnoreCase(op)) {
-         if(b==0){
-         System.out.println("Cannot divide by zero");
-         }
-         else{
-         div = a/b;
-         System.out.println("Division of the given numbers results in: " + div);
-         }
-   }
-
-   else{
-        System.out.println("Error! Try again");
-
-   }
- }
 }
